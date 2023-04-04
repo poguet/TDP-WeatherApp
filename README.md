0078178ad4d7d9ea2ca745cc96b4110b754670d3
poguet <pogue12@ymail.com>
2023-04-03 23:10:15 -0500

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# TDP-WeatherApp
+# WeatherApp
+ link - 
+## description
+
+This application allows a user to view the current tempature, wind speed, and humidity along with a future 5 day forecast of the same conditions 
+
+I liked the results of the application and that they are very helpful to the user when needed 
+
+this challange was very difficult for me. I had several difficult times with the API.
+
+Java script has been a real challange for me to understand. completeing this challange has added to my confidence in the skills I have learned.how 
+
+## screen shot
+<img width="1503" alt="Screenshot 2023-04-03 at 11 02 15 PM" src="https://user-images.githubusercontent.com/118129483/229684490-ff2d8d85-64bc-4ba0-aefc-7b704d786ce0.png">
+
+## Github
+ https://github.com/poguet/TDP-WeatherApp
--- assets/script.js
+++ assets/script.js
@@ -34,7 +34,30 @@ function showforecastdata(cityName){
 let Url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${api_key}&units=imperial`;
 
 fetch(Url).then(res=>res.json()).then(data=>{
-    console.log(data)
+    //console.log(data.list)
+    console.log(data.list[0].main.temp)
+    console.log(data.list[0].wind.speed)
+    console.log(data.list[0].main.humidity)
+
+    forecastdata.style.border = '1px solid black'
+
+    var forcastList=data.list
+    for (var i = 0; i < forcastList.length;i+=8) {
+        console.log(forcastList[i])
+        console.log(forcastList[i].main.temp)
+
+        let tempEl = document.createElement('p')
+        tempEl.innerHTML = `Tempature: ${forcastList[i].main.temp}`
+        forecastdata.append(tempEl)
+
+        let windEl = document.createElement('p')
+        windEl.innerHTML = `Wind Speed ${forcastList[i].wind.speed}`
+        forecastdata.append(windEl)
+
+        let humidityEl = document.createElement('p')
+        humidityEl.innerHTML = `humidity ${forcastList[i].main.humidity}`
+        forecastdata.append(humidityEl)
+    }
 })
 }
 
